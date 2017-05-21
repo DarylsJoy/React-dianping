788a64bebd2e0fb9e03ac0a57123456e33deda47
董鑫强 <joynjoy@163.com>
2017-05-21 16:42:25 +0800

    Update webpack.production.config.js

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -43,7 +43,7 @@ module.exports = {
 
   plugins: [
     // webpack 内置的 banner-plugin
-    new webpack.BannerPlugin("Copyright by wangfupeng1988@github.com."),
+    new webpack.BannerPlugin("Copyright by joynjoy@163.com"),
 
     // html 模板插件
     new HtmlWebpackPlugin({
